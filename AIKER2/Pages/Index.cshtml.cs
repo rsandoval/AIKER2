c57d7caa528e6ce6b298e9523c5923f0ac2906de
Rodrigo Sandoval <rodrigo@rsolver.com>
2026-06-21 17:39:16 -0400

    First commit using a web app .NET Core Blazor Pages Web App template

---
+++ AIKER2/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AIKER2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
